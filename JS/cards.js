2f18c1fdfeb21ccd72a6b63ee3e3b43748e458af
James Maroko <jamiemaroko41@gmail.com>
2024-10-20 08:39:50 +0300

    Updated files

--- JS/cards.js
+++ JS/cards.js
@@ -1,318 +1,318 @@
 //Full array of possible cards
-let cards;
+var cards;
 
 function getCards(){
 	cards = [
 		{ 
 			"suit": "clubs",
 			"name": "2",
-			"src": "2_of_clubs.png",
+			"src": "Clubs-2.png",
 			"value": 2
 		},
 		{ 
 			"suit": "clubs",
 			"name": "3",
-			"src": "3_of_clubs.png",
+			"src": "Clubs-3.png",
 			"value": 3
 		},
 		{ 
 			"suit": "clubs",
 			"name": "4",
-			"src": "4_of_clubs.png",
+			"src": "Clubs-4.png",
 			"value": 4
 		},
 		{ 
 			"suit": "clubs",
 			"name": "5",
-			"src": "5_of_clubs.png",
+			"src": "Clubs-5.png",
 			"value": 5
 		},
 		{ 
 			"suit": "clubs",
 			"name": "6",
-			"src": "6_of_clubs.png",
+			"src": "Clubs-6.png",
 			"value": 6
 		},
 		{ 
 			"suit": "clubs",
 			"name": "7",
-			"src": "7_of_clubs.png",
+			"src": "Clubs-7.png",
 			"value": 7
 		},
 		{ 
 			"suit": "clubs",
 			"name": "8",
-			"src": "8_of_clubs.png",
+			"src": "Clubs-8.png",
 			"value": 8
 		},
 		{ 
 			"suit": "clubs",
 			"name": "9",
-			"src": "9_of_clubs.png",
+			"src": "Clubs-9.png",
 			"value": 9
 		},
 		{ 
 			"suit": "clubs",
 			"name": "10",
-			"src": "10_of_clubs.png",
+			"src": "Clubs-10.png",
 			"value": 10
 		},
 		{ 
 			"suit": "clubs",
 			"name": "ace",
-			"src": "ace_of_clubs.png",
+			"src": "Clubs-Ace.png",
 			"value": 11
 		},
 		{ 
 			"suit": "clubs",
 			"name": "jack",
-			"src": "jack_of_clubs.png",
+			"src": "Clubs-Jack.png",
 			"value": 10
 		},
 		{ 
 			"suit": "clubs",
 			"name": "king",
-			"src": "king_of_clubs.png",
+			"src": "Clubs-King.png",
 			"value": 10
 		},
 		{ 
 			"suit": "clubs",
 			"name": "queen",
-			"src": "queen_of_clubs.png",
+			"src": "Clubs-Queen.png",
 			"value": 10
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "2",
-			"src": "2_of_diamonds.png",
+			"src": "Diamond-2.png",
 			"value": 2
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "3",
-			"src": "3_of_diamonds.png",
+			"src": "Diamond-3.png",
 			"value": 3
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "4",
-			"src": "4_of_diamonds.png",
+			"src": "Diamond-4.png",
 			"value": 4
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "5",
-			"src": "5_of_diamonds.png",
+			"src": "Diamond-5.png",
 			"value": 5
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "6",
-			"src": "6_of_diamonds.png",
+			"src": "Diamond-6.png",
 			"value": 6
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "7",
-			"src": "7_of_diamonds.png",
+			"src": "Diamond-7.png",
 			"value": 7
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "8",
-			"src": "8_of_diamonds.png",
+			"src": "Diamond-8.png",
 			"value": 8
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "9",
-			"src": "9_of_diamonds.png",
+			"src": "Diamond-9.png",
 			"value": 9
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "10",
-			"src": "10_of_diamonds.png",
+			"src": "Diamond-10.png",
 			"value": 10
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "ace",
-			"src": "ace_of_diamonds.png",
+			"src": "Diamond-Ace.png",
 			"value": 11
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "jack",
-			"src": "jack_of_diamonds.png",
+			"src": "Diamond-Jack.png",
 			"value": 10
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "king",
-			"src": "king_of_diamonds.png",
+			"src": "Diamond-King.png",
 			"value": 10
 		},
 		{ 
 			"suit": "diamonds",
 			"name": "queen",
-			"src": "queen_of_diamonds.png",
+			"src": "Diamond-Queen.png",
 			"value": 10
 		},
 		{ 
 			"suit": "hearts",
 			"name": "2",
-			"src": "2_of_hearts.png",
+			"src": "Hearts-2.png",
 			"value": 2
 		},
 		{ 
 			"suit": "hearts",
 			"name": "3",
-			"src": "3_of_hearts.png",
+			"src": "Hearts-3.png",
 			"value": 3
 		},
 		{ 
 			"suit": "hearts",
 			"name": "4",
-			"src": "4_of_hearts.png",
+			"src": "Hearts-4.png",
 			"value": 4
 		},
 		{ 
 			"suit": "hearts",
 			"name": "5",
-			"src": "5_of_hearts.png",
+			"src": "Hearts-5.png",
 			"value": 5
 		},
 		{ 
 			"suit": "hearts",
 			"name": "6",
-			"src": "6_of_hearts.png",
+			"src": "Hearts-6.png",
 			"value": 6
 		},
 		{ 
 			"suit": "hearts",
 			"name": "7",
-			"src": "7_of_hearts.png",
+			"src": "Hearts-7.png",
 			"value": 7
 		},
 		{ 
 			"suit": "hearts",
 			"name": "8",
-			"src": "8_of_hearts.png",
+			"src": "Hearts-8.png",
 			"value": 8
 		},
 		{ 
 			"suit": "hearts",
 			"name": "9",
-			"src": "9_of_hearts.png",
+			"src": "Hearts-9.png",
 			"value": 9
 		},
 		{ 
 			"suit": "hearts",
 			"name": "10",
-			"src": "10_of_hearts.png",
+			"src": "Hearts-10.png",
 			"value": 10
 		},
 		{ 
 			"suit": "hearts",
 			"name": "ace",
-			"src": "ace_of_hearts.png",
+			"src": "Hearts-Ace.png",
 			"value": 11
 		},
 		{ 
 			"suit": "hearts",
 			"name": "jack",
-			"src": "jack_of_hearts.png",
+			"src": "Hearts-Jack.png",
 			"value": 10
 		},
 		{ 
 			"suit": "hearts",
 			"name": "king",
-			"src": "king_of_hearts.png",
+			"src": "Hearts-King.png",
 			"value": 10
 		},
 		{ 
 			"suit": "hearts",
 			"name": "queen",
-			"src": "queen_of_hearts.png",
+			"src": "Hearts-Queen.png",
 			"value": 10
 		},
 		{ 
 			"suit": "spades",
 			"name": "2",
-			"src": "2_of_spades.png",
+			"src": "Spades-2.png",
 			"value": 2
 		},
 		{ 
 			"suit": "spades",
 			"name": "3",
-			"src": "3_of_spades.png",
+			"src": "Spades-3.png",
 			"value": 3
 		},
 		{ 
 			"suit": "spades",
 			"name": "4",
-			"src": "4_of_spades.png",
+			"src": "Spades-4.png",
 			"value": 4
 		},
 		{ 
 			"suit": "spades",
 			"name": "5",
-			"src": "5_of_spades.png",
+			"src": "Spades-5.png",
 			"value": 5
 		},
 		{ 
 			"suit": "spades",
 			"name": "6",
-			"src": "6_of_spades.png",
+			"src": "Spades-6.png",
 			"value": 6
 		},
 		{ 
 			"suit": "spades",
 			"name": "7",
-			"src": "7_of_spades.png",
+			"src": "Spades-7.png",
 			"value": 7
 		},
 		{ 
 			"suit": "spades",
 			"name": "8",
-			"src": "8_of_spades.png",
+			"src": "Spades-8.png",
 			"value": 8
 		},
 		{ 
 			"suit": "spades",
 			"name": "9",
-			"src": "9_of_spades.png",
+			"src": "Spades-9.png",
 			"value": 9
 		},
 		{ 
 			"suit": "spades",
 			"name": "10",
-			"src": "10_of_spades.png",
+			"src": "Spades-10.png",
 			"value": 10
 		},
 		{ 
 			"suit": "spades",
 			"name": "ace",
-			"src": "ace_of_spades.png",
+			"src": "Spades-Ace.png",
 			"value": 11
 		},
 		{ 
 			"suit": "spades",
 			"name": "jack",
-			"src": "jack_of_spades.png",
+			"src": "Spades-Jack.png",
 			"value": 10
 		},
 		{ 
 			"suit": "spades",
 			"name": "king",
-			"src": "king_of_spades.png",
+			"src": "Spades-King.png",
 			"value": 10
 		},
 		{ 
 			"suit": "spades",
 			"name": "queen",
-			"src": "queen_of_spades.png",
+			"src": "Spades-Queen.png",
 			"value": 10
 		}
 	]
